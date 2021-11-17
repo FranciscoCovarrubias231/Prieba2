d6494aa246f14075ac7a8b6a22beacbdbbcd50b7
Francisco Covarrubias Lamadrid <94408904+FranciscoCovarrubias231@users.noreply.github.com>
2021-11-17 12:40:56 -0600

    Acabo de agregar mi nombre

--- src/preba2/Preba2.java
+++ src/preba2/Preba2.java
@@ -1,24 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package preba2;
 
-/**
- *
- * @author FRANCISCO
- */
 public class Preba2 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args)
     {
        System.out.println("Adios");
        System.out.println("Hasta luego");
        System.out.println("Zayonara");
+        System.out.println("Jesus eduardo");
     }
     
 }
